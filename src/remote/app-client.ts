a6ef7f940a2ef3032b8d1e6a352a2fd272074f36
PaulgSmith <PaulSmith.Contactinfo@gmail.com>
2023-04-20 00:31:58 -0700

    Update app-client.ts

--- src/remote/app-client.ts
+++ src/remote/app-client.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const appClient = axios.create({
-    baseURL: 'ec2-54-71-28-118.us-west-2.compute.amazonaws.com:3000',
+    baseURL: 'http://ec2-54-71-28-118.us-west-2.compute.amazonaws.com:3000',
     headers: {
         'Content-Type': 'application/json'
     }
