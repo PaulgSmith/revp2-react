8ff5e03c363d54ed5ea9a08670aae9e43059c01b
PaulgSmith <PaulSmith.Contactinfo@gmail.com>
2023-04-20 00:00:14 -0700

    Update app-client.ts
    got rid of local host

--- src/remote/app-client.ts
+++ src/remote/app-client.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const appClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'ec2-54-71-28-118.us-west-2.compute.amazonaws.com:3000',
     headers: {
         'Content-Type': 'application/json'
     }
