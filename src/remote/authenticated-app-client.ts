342fe1c9ad8422ee11f5b13b1c6c38996a9b524b
PaulgSmith <PaulSmith.Contactinfo@gmail.com>
2023-04-20 00:06:31 -0700

    Update authenticated-app-client.ts
    I AM DUMB

--- src/remote/authenticated-app-client.ts
+++ src/remote/authenticated-app-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authAppClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'ec2-54-71-28-118.us-west-2.compute.amazonaws.com:3000',
     headers: {
         'Accept': "application/json",
     }
